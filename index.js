5f2230c6b0952c4165ee496b9a0f0ae649cb2d00
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2024-11-06 23:12:43 +0000

    UPDATE: Fixed vulnerabilities and tweaked the code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  contains-vowels - Checks if a string contains vowels
- *  @version: v1.0.2
+ *  @version: v1.0.3
  *  @link: https://github.com/tutyamxx/contains-vowels
  *  @license: GPL v3.0
  **/
