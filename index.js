1086528da61ee1c2307d3d6dbf0bb9806e317567
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2025-01-29 15:43:44 +0000

    UPDATE: Updated dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  contains-vowels - Checks if a string contains vowels
- *  @version: v1.0.3
+ *  @version: v1.0.4
  *  @link: https://github.com/tutyamxx/contains-vowels
  *  @license: GPL v3.0
  **/
