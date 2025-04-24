91f1077be23f73aec955181ee99cd5dbbee6b604
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2025-04-24 16:55:02 +0100

    UPDATE: Optimisations

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  *  contains-vowels - Checks if a string contains vowels
- *  @version: v1.0.4
+ *  @version: v1.0.5
  *  @link: https://github.com/tutyamxx/contains-vowels
  *  @license: GPL v3.0
  **/
 
-module.exports = (paramString) => (paramString && paramString?.length > 0 && typeof paramString === 'string') ? /[aeiouAEIOU]/.test(paramString) : false;
+module.exports = (paramString) => (typeof paramString === 'string' && paramString.length > 0) ? /[aeiou]/i.test(paramString) : false;
